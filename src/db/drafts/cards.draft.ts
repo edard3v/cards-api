fcc127598dfe2688c4b0cf667cfc97d0b3d1c48e
edard3v <edard3v@gmail.com>
2025-02-09 04:11:08 -0500

    refactor

--- src/db/drafts/cards.draft.ts
+++ src/db/drafts/cards.draft.ts
@@ -5,8 +5,7 @@ export const CARDS = [
     front: "dormir",
     back: "sleep",
     img: "https://tse2.mm.bing.net/th?id=OIP.OGvIcCM3871BpzNmWntdAQHaE8&pid=Api",
-    audio:
-      "https://media.merriam-webster.com/audio/prons/en/us/mp3/s/sleep001.mp3",
+    audio: "https://media.merriam-webster.com/audio/prons/en/us/mp3/s/sleep001.mp3",
     categoryId: "c1000000-0000-0000-0000-000000000000",
     authorId: "a1000000-0000-0000-0000-000000000000",
   },
@@ -16,8 +15,7 @@ export const CARDS = [
     front: "comer",
     back: "eat",
     img: "https://images.pexels.com/photos/1537635/pexels-photo-1537635.jpeg?auto=compress&cs=tinysrgb&w=500&h=500&dpr=1",
-    audio:
-      "https://media.merriam-webster.com/audio/prons/en/us/mp3/e/eat00001.mp3",
+    audio: "https://media.merriam-webster.com/audio/prons/en/us/mp3/e/eat00001.mp3",
     categoryId: "c1000000-0000-0000-0000-000000000000",
     authorId: "a1000000-0000-0000-0000-000000000000",
   },
@@ -27,8 +25,7 @@ export const CARDS = [
     front: "leer",
     back: "read",
     img: "https://images.pexels.com/photos/590493/pexels-photo-590493.jpeg?auto=compress&cs=tinysrgb&w=500&h=500&dpr=1",
-    audio:
-      "https://media.merriam-webster.com/audio/prons/en/us/mp3/r/read0001.mp3",
+    audio: "https://media.merriam-webster.com/audio/prons/en/us/mp3/r/read0001.mp3",
     categoryId: "c1000000-0000-0000-0000-000000000000",
     authorId: "a1000000-0000-0000-0000-000000000000",
   },
@@ -37,19 +34,9 @@ export const CARDS = [
     name: "escribir",
     front: "escribir",
     back: "write",
-    img: "https://tse2.mm.bing.net/th?id=OIP.OGvIcCM3871BpzNmWntdAQHaE8&pid=Api",
-    audio:
-      "https://media.merriam-webster.com/audio/prons/en/us/mp3/w/write001.mp3",
+    img: "https://images.pexels.com/photos/29052901/pexels-photo-29052901/free-photo-of-trabajo-remoto-en-un-entorno-tranquilo-al-aire-libre.jpeg?auto=compress&cs=tinysrgb&w=500&h=500&dpr=1",
+    audio: "https://media.merriam-webster.com/audio/prons/en/us/mp3/w/write001.mp3",
     categoryId: "c1000000-0000-0000-0000-000000000000",
     authorId: "a2000000-0000-0000-0000-000000000000",
   },
-  // {
-  //   id: "ca500000-0000-0000-0000-000000000000",
-  //   front: "correr",
-  //   back: "run",
-  //   img: "https://images.pexels.com/photos/5965907/pexels-photo-5965907.jpeg?auto=compress&cs=tinysrgb&w=500&h=500&dpr=1",
-  //   audio:
-  //     "https://media.merriam-webster.com/audio/prons/en/us/mp3/r/run001.mp3",
-  //   categoryId: "c1000000-0000-0000-0000-000000000000",
-  // },
 ];
